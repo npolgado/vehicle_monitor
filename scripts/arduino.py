d32fd17de84bd8725d9b2215b74a297eb36114ba
Nick <nicholas831@gmail.com>
2023-10-27 01:46:54 -0700

    broken rideheight node, needs help

--- scripts/arduino.py
+++ scripts/arduino.py
@@ -9,23 +9,23 @@ ARDUINO_STOP_BIT = 0xABC
 def parse_serial_data(ser):
     rospy.init_node('serial_reader_node', anonymous=True)
     
-    # Create a dictionary to map wheel names to their corresponding masks
-    wheel_topics = {
-        "front_left": 0xFFF,
-        "front_right": 0xFFF000,
-        "rear_left": 0xFFF000000,
-        "rear_right": 0xFFF000000000,
-    }
+    # # Create a dictionary to map wheel names to their corresponding masks
+    # wheel_topics = {
+    #     "front_left": 0xFFF,
+    #     "front_right": 0xFFF000,
+    #     "rear_left": 0xFFF000000,
+    #     "rear_right": 0xFFF000000000,
+    # }
 
     # Create publishers for each wheel topic
-    publishers = {}
-    for wheel, mask in wheel_topics.items():
-        topic_name = "rideheight/" + wheel
-        publishers[wheel] = rospy.Publisher(topic_name, Int16, queue_size=10)
+    # publishers = {}
+    # for wheel, mask in wheel_topics.items():
+    #     topic_name = "rideheight/" + wheel
+    #     publishers[wheel] = rospy.Publisher(topic_name, Int16, queue_size=10)
 
     stop_bit_received = False
     while not rospy.is_shutdown() and not stop_bit_received:
-        data = ser.read(8)  # Assuming 8 bytes for Int64 data
+        data = ser.readLine(8)  # Assuming 8 bytes for Int64 data
         if len(data) == 8:
             int64_value = int.from_bytes(data, byteorder='little', signed=True)
             for wheel, mask in wheel_topics.items():
